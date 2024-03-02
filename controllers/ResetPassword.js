7b9f15a89bb726786d4258c0439d399ac96bf1a6
HarshalMjn <mahajanh006@gmail.com>
2024-03-02 22:15:59 +0530

    fix bug

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,53 +1,54 @@
 const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+
 
 
 //resetPasswordToken
 exports.resetPasswordToken = async (req, res) => {
-    try{
-        //get email from req body
-        const email = req.body.email;
-        //check user this email, email validation
-        const user = await User.findOne({email:email});
-        if(!user) {
-            return res.json({
-                success:false,
-                message:"Your Email is not registered with us"
-            
-            });
-        }
-        //generate token 
-        const token = crypto.randomUUID();
-      //update user by adding token and expiration time
-      const updateDetails = await User.findOneAndUpdate(
-                                        {email:email},
-                                        {
-                                            token:token,
-                                            resetPasswordExpires:  Date.now() + 5*60*1000,
-                                        },
-                                        {new:true});
-      //create url
-      const url = `http://localhost:3000/update-password/${token}`
-      //send mail containing the url
-      await mailSender(email,
-                       "Password Reset Link",
-                       `Password Reset Link: ${url}`)
-      //return response
-      return res.json({
-        success:true,
-        message:'Email sent successfully, please check email and change pwd',
-      })
+	try {
+		const email = req.body.email;
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			return res.json({
+				success: false,
+				message: `This Email: ${email} is not Registered With Us Enter a Valid Email `,
+			});
+		}
+		const token = crypto.randomBytes(20).toString("hex");
 
-    } catch(error) {
-        console.log(error);
-        return res.status(500).json({
-            success:false,
-            message:'Something went wrong while sending reset pwd mail'
-        })
+		const updatedDetails = await User.findOneAndUpdate(
+			{ email: email },
+			{
+				token: token,
+				resetPasswordExpires: Date.now() + 3600000,
+			},
+			{ new: true }
+		);
+		console.log("DETAILS", updatedDetails);
 
-    }
-    
-}
+		const url = `http://localhost:3000/update-password/${token}`;
+
+		await mailSender(
+			email,
+			"Password Reset",
+			`Your Link for email verification is ${url}. Please click this url to reset your password.`
+		);
+
+		res.json({
+			success: true,
+			message:
+				"Email Sent Successfully, Please Check Your Email to Continue Further",
+		});
+	} catch (error) {
+		return res.json({
+			error: error.message,
+			success: false,
+			message: `Some Error in Sending the Reset Message`,
+		});
+	}
+};
 
 
 //resetPassword
